17de969e41be59f01c4a438760cd7dd6c7e0109c
Sanath <sanath50@gmail.com>
2023-01-12 23:32:01 +0530

    AB#Kart

--- VyasKart/shop/urls.py
+++ VyasKart/shop/urls.py
@@ -4,5 +4,11 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.index, name="ShopHome")
+    path("", views.index, name="ShopHome"),
+    path("about/", views.about, name="AboutUs"),
+    path("contact/", views.contact, name="ContactUs"),
+    path("tracker/", views.tracker, name="trackingStatus"),
+    path("search/", views.search, name="Search"),
+    path("productview/", views.prodview  , name="Search"),
+    path("checkout/", views.checkout, name="Checkout"),
 ]
--- VyasKart/shop/views.py
+++ VyasKart/shop/views.py
@@ -9,3 +9,18 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request,'shop/index.html')
+
+def about(request):
+    return HttpResponse("This is About Us")
+
+def contact(request):
+    return HttpResponse("This is contact")
+def tracker(request):
+    return HttpResponse("This is tracker")
+def search(request):
+    return HttpResponse("This is search")
+
+def prodview(request):
+    return HttpResponse("This is prodview")
+def checkout(request):
+    return HttpResponse("This is checkout")
