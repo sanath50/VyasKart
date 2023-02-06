3741d1b26e180cd0c5ff161c09821f46385a1a74
Sanath <sanath50@gmail.com>
2023-02-06 23:44:58 +0530

    AB#Kart

--- VyasKart/shop/models.py
+++ VyasKart/shop/models.py
@@ -1,15 +1,38 @@
 from django.db import models
-
 # Create your models here.
 class Product(models.Model):
     product_id = models.AutoField
     product_name = models.CharField(max_length=50)
-    category = models.CharField(max_length=50,default = "")
-    subcategory = models.CharField(max_length=50,default = "")
+    category = models.CharField(max_length=50, default = "")
+    subcategory = models.CharField(max_length=50, default = "")
     price = models.IntegerField(default = 0)
     desc = models.CharField(max_length=300)
     pub_date = models.DateField()
-    image = models.ImageField(upload_to="shop/images",default="")
+    image = models.ImageField(upload_to="shop/images", default="")
 
     def __str__(self):
         return self.product_name
+
+class Contact(models.Model):
+    message_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100, default= "")
+    phone = models.CharField(max_length=100, default = "")
+    desc = models.CharField(max_length=300)
+
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    order_id = models.AutoField(primary_key=True)
+    items_json = models.CharField(max_length=5000)
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    state = models.CharField(max_length=200)
+    zip_code = models.CharField(max_length=200)
+    phone = models.CharField(max_length=200,default = "")
+
--- VyasKart/shop/views.py
+++ VyasKart/shop/views.py
@@ -2,7 +2,7 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product
+from .models import Product, Contact, Order
 from math import ceil
 # Create your views here.
 
@@ -25,7 +25,15 @@ def about(request):
     return render(request, 'shop/about.html')
 
 def contact(request):
-
+    # if request.method == "POST":
+    #     print(request)
+    name = request.POST.get('name','')
+    email = request.POST.get('email','')
+    phone = request.POST.get('phone','')
+    desc = request.POST.get('desc','')
+    # print(name,email,phone,desc)
+    contact = Contact(name=name, email=email, phone=phone, desc=desc)
+    contact.save()
     return render(request, 'shop/contact.html')
 
 def tracker(request):
@@ -41,4 +49,19 @@ def productView(request, myid):
     return render(request, "shop/prodView.html", {'product': product[0]})
 
 def checkout(request):
+    if request.method=="POST":
+        items_json= request.POST.get('itemsJson', '')
+        name=request.POST.get('name', '')
+        email=request.POST.get('email', '')
+        address=request.POST.get('address1', '') + " " + request.POST.get('address2', '')
+        city=request.POST.get('city', '')
+        state=request.POST.get('state', '')
+        zip_code=request.POST.get('zip_code', '')
+        phone=request.POST.get('phone', '')
+
+        order = Order(items_json= items_json, name=name, email=email, address= address, city=city, state=state, zip_code=zip_code, phone=phone)
+        order.save()
+        thank=True
+        id=order.order_id
+        return render(request, 'shop/checkout.html', {'thank':thank, 'id':id})
     return render(request, 'shop/checkout.html')
